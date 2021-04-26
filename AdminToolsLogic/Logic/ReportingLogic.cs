a1c8b5219eb2e0178bdae62946ab4b25c3058d8f
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-04-26 18:11:34 +0300

    changes logic project name. adds an enum

---
+++ AdminToolsLogic/Logic/ReportingLogic.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using AdminLogic.LogicModels;
+
+namespace AdminLogic.Logic
+{
+    class ReportingLogic
+    {
+        public async Task<bool> CreateReportTicket(ReportType reportType, string id, string description)
+        {
+            return false;
+        }
+    }
+}
---
+++ AdminToolsLogic/LogicModels/ReportType.cs
@@ -0,0 +1,11 @@
+namespace AdminLogic.LogicModels
+{
+    enum ReportType
+    {
+        Review,
+        User,
+        Movie,
+        Group,
+        Forum
+    }
+}
