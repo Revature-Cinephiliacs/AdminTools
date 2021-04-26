819acc2271b8ef76be6acaaabcb399a56ca98a03
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-04-26 20:05:02 +0300

    adds references for testing project.

--- AdminToolsTests/UnitTest1.cs
+++ AdminToolsTests/UnitTest1.cs
@@ -5,9 +5,23 @@ namespace AdminToolsTests
 {
     public class UnitTest1
     {
+        readonly DbContextOptions<Cinephiliacs_UserContext> options = new DbContextOptionsBuilder<Cinephiliacs_UserContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
+
         [Fact]
         public void Test1()
         {
+            using (var context = new Cinephiliacs_UserContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            using (var context1 = new Cinephiliacs_UserContext(options))
+            {
+                context1.Database.EnsureCreated();
+            }
 
         }
     }
