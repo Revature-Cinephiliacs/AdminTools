78bb15ab19d2b705c4a29b747d0ccc4db93f3488
rwplyler <56159501+rwplyler@users.noreply.github.com>
2021-04-28 18:05:04 -0400

    Added Comment

--- Repository/Models/AffectedSubject.cs
+++ Repository/Models/AffectedSubject.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace Repository.Models
 {
+    //Affect subjects is a list of possible topics to use for reports
     public partial class AffectedSubject
     {
         public string AffectedSubject1 { get; set; }
